3ae14be80f399149bd0e3bce01b400f3a360ff25
Supakit Dangprasert <dekseed3@gmail.com>
2023-04-21 10:01:54 +0700

    Update last time

--- resources/views/pages/admin/manage_users/permissions/index.blade.php
+++ resources/views/pages/admin/manage_users/permissions/index.blade.php
@@ -178,16 +178,7 @@
                     <h2 class="fw-bolder">สิทธิ์การใช้งานขั้นพื้นฐาน</h2>
                     <!--end::Modal title-->
                     <!--begin::Close-->
-                    <div id="kt_modal_add_user_close" class="btn btn-icon btn-sm btn-active-icon-primary">
-                        <!--begin::Svg Icon | path: icons/duotune/arrows/arr061.svg-->
-                        <span class="svg-icon svg-icon-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1" transform="rotate(-45 6 17.3137)" fill="black" />
-                                <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)" fill="black" />
-                            </svg>
-                        </span>
-                        <!--end::Svg Icon-->
-                    </div>
+                    <button type="button" class="close" data-dismiss="modal"><span>&times;</span>
                     <!--end::Close-->
                 </div>
                 <!--end::Modal header-->
